fe884e0ad4221cd0211172c9af9e7846a279f833
Abdulsamad <almuhandith1497@gmail.com>
2023-06-27 18:53:25 +0100

    handle response

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function handleResponseFromAPI(promise) {
+    const myPromise = new Promise((resolve, reject) => {
+        if (promise) {
+            resolve({
+                status: 200,
+                body: 'success'
+            });
+            console.log("Got a response from the API")
+        }
+        else {
+            reject(Error())
+        }
+    })
+
+    return myPromise;
+}
