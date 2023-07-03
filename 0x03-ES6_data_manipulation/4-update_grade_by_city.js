43422960ae60905eed787afe6bd915dc6e1232e1
Abdulsamad <almuhandith1497@gmail.com>
2023-07-03 14:45:13 +0100

    typed array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,6 @@
-export default function updateStudentGradeByCity() {
-  
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents.filter((student) => student.location === city).map((student) => {
+    const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+    return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
+  });
 }
