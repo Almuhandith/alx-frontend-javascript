0c7faec28618bba1047f1ca80acc981a0ada061a
Abdulsamad <almuhandith1497@gmail.com>
2023-07-04 14:33:01 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,10 @@
 export default function cleanSet(set, startString) {
-
+  if (!startString || !startString.length) return '';
+  let values = '';
+  for (const i of set) {
+    if (i && i.startsWith(startString)) {
+      values += values.length === 0 ? i.replace(startString, '') : i.replace(startString, '-');
+    }
+  }
+  return values;
 }
